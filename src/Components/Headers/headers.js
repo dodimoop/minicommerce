96f020bbb934beabb38c45f89090aca23c546667
dodimoop <dody.setiyawan11@gmail.com>
2019-03-15 04:27:05 +0700

    Add header

---
+++ src/Components/Headers/headers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header, Segment } from 'semantic-ui-react'
+
+const HeaderComponent = () => (
+  <Segment clearing>
+    <Header as='h2' floated='right'>
+      Minicommerce
+    </Header>
+  </Segment>
+)
+
+export default HeaderComponent
