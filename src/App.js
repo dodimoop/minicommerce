acd2283ae074ae92f8509bce0660563703e2c1af
dodimoop <dody.setiyawan11@gmail.com>
2019-03-15 18:58:14 +0700

    Add Route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './app.module.scss';
 import HeaderComponent from './Components/Headers/headers';
 import CardComponent from './Components/Cards/cards';
+import DetailsCard from './Components/DetailsCard/detailsCard';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderComponent />
-        <CardComponent />
+        <Switch>
+          <Route path="/" exact component={CardComponent} />
+          <Route path="/product/:id" component={DetailsCard} />
+          <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
       </div>
     );
   }
