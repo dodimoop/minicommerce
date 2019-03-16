3989ce6c697c7a46a003a8a0bbb695e7faea53c3
dodimoop <dody.setiyawan11@gmail.com>
2019-03-16 10:18:45 +0700

    remove console and  and add button back to catalog

--- src/Components/DetailsCard/detailsCard.js
+++ src/Components/DetailsCard/detailsCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Grid, Image, Card } from 'semantic-ui-react'
+import { Container, Grid, Image, Segment, Header, Icon, Button,Card } from 'semantic-ui-react'
 import { BASE_MEDIA } from '../../Services/env'
 import classes from './detailsCard.module.scss';
 
@@ -9,12 +9,14 @@ class detailsCard extends Component {
   }
 
   async componentDidMount () {
-    // console.log(window.localStorage.getItem('currentProduct'));
     await this.setState({
       currentProduct: JSON.parse(window.localStorage.getItem('currentProduct'))
     })
-    // console.log(this.state.currentProduct);
   }
+
+  onBackToCatalog = async () => {
+    this.props.history.push('/')
+  } 
 
   render() {
 
@@ -25,14 +27,30 @@ class detailsCard extends Component {
           <Grid.Column width={6}>
             <Image className={classes.detailsCard} src={BASE_MEDIA + this.state.currentProduct.img.name} />
           </Grid.Column>
-          <Grid.Column width={10}>
-          <Card
-            header={this.state.currentProduct.title}
-            meta={'USD ' + this.state.currentProduct.pricing.price}
-            description={this.state.currentProduct.desc}
-            className={classes.detailsContent}
-          />
-          </Grid.Column>
+          <Grid.Column className="gridColumn" width={10}>
+            <Grid.Column className={classes.detailsCard} >
+              <Header as="h1" dividing>
+                {this.state.currentProduct.title}
+              </Header>
+              <Icon className={classes.iconPrice} name="dollar sign"/> <b>{this.state.currentProduct.pricing.price}</b>
+              <Segment className={classes.detailsDescription}>
+                <Header as="h3">
+                  <b>Description</b>
+                </Header>
+                <Container>
+                  <p>{this.state.currentProduct.desc}</p>
+                </Container>
+              </Segment>
+            </Grid.Column>
+            <Button
+              content='Back to Catalogs'
+              icon='left arrow'
+              labelPosition='left'
+              floated="right"
+              onClick={this.onBackToCatalog}
+              color="google plus" 
+            />
+          </Grid.Column>         
         </Grid>
       )
     }
